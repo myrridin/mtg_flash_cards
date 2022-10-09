8a33dc3d406e6b0cac273f755a7e7dd6b502dcd6
Thomas Hart II <myrridin@gmail.com>
2022-10-09 19:06:05 -0400

    Add ability to fetch bulk data from Scryfall

--- api/app/services/import_card_data.rb
+++ api/app/services/import_card_data.rb
@@ -1,37 +1,56 @@
 class ImportCardData
   CARD_DATA_DIR = "card_data"
+  SCRYFALL_BULK_DATA_URI = "https://api.scryfall.com/bulk-data"
 
   def self.call
-    old_logger = ActiveRecord::Base.logger
-    ActiveRecord::Base.logger = nil
-
-    Dir["#{CARD_DATA_DIR}/**.json"].each do |filename|
-      import_file(filename)
-    end
-
-    ActiveRecord::Base.logger = old_logger
+    process_data(all_card_data)
   end
 
   private
 
-  def self.import_file(filename)
-    file_data = File.read(filename)
-    data = JSON.parse(file_data)
-    insert_data = data.map { |card_data| process_card(card_data) }
+  def self.process_data(all_card_data)
+    insert_data = all_card_data.map { |card_data| process_card(card_data) }
+
+    Rails.logger.info { "Inserting Cards" }
+    old_logger = ActiveRecord::Base.logger
+    ActiveRecord::Base.logger = nil
+
     Card.insert_all(insert_data)
+
+    ActiveRecord::Base.logger = old_logger
+    Rails.logger.info { "Done inserting Cards" }
+  end
+
+  def self.card_data_uri
+    @card_data_uri ||= get_card_data_uri
+  end
+
+  def self.get_card_data_uri
+    Rails.logger.info { "Getting card data URI" }
+    bulk_data_files = FastJsonparser.parse(RestClient.get(SCRYFALL_BULK_DATA_URI))
+    file_uri_hash = bulk_data_files[:data].find { |h| h[:type] == "default_cards" }
+    Rails.logger.info { "Got card data URI #{file_uri_hash[:download_uri]}" }
+    file_uri_hash[:download_uri]
+  end
+
+  def self.all_card_data
+    Rails.logger.info { "Getting card data" }
+    file_data_json = Net::HTTP.get(URI.parse(card_data_uri))
+    Rails.logger.info { "Got card data" }
+    FastJsonparser.parse(file_data_json)
   end
 
   def self.process_card(card_data)
     {
-      scryfall_id: card_data["id"],
-      tcgplayer_id: card_data["tcgplayer_id"],
-      mana_cost: card_data["mana_cost"],
-      name: card_data["name"],
-      type_line: card_data["type_line"],
-      oracle_text: card_data["oracle_text"],
-      set_code: card_data["set"],
-      artist: card_data["artist"],
-      image_url: card_data.dig("image_uris", "png")
+      scryfall_id: card_data[:id],
+      tcgplayer_id: card_data[:tcgplayer_id],
+      mana_cost: card_data[:mana_cost],
+      name: card_data[:name],
+      type_line: card_data[:type_line],
+      oracle_text: card_data[:oracle_text],
+      set_code: card_data[:set],
+      artist: card_data[:artist],
+      image_url: card_data.dig(:image_uris, :large)
     }
   end
 end
